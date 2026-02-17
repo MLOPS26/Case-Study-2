a7e56bd7d8f7db09fc922c58718619e37c52820d
connor-jason <connorejason@gmail.com>
2026-02-17 16:18:23 -0500

    user group09

--- services/add-cron.sh
+++ services/add-cron.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # sudo bash services/add-cron.sh
 
-chmod +x /home/ubuntu/Case-Study-2/services/watchdog.sh
+chmod +x /home/group09/Case-Study-2/services/watchdog.sh
 
-echo "* * * * * root /home/ubuntu/Case-Study-2/services/watchdog.sh" > /etc/cron.d/case-study-2-watchdog
+echo "* * * * * root /home/group09/Case-Study-2/services/watchdog.sh" > /etc/cron.d/case-study-2-watchdog
 chmod 644 /etc/cron.d/case-study-2-watchdog
 
 echo "cron job installed"
