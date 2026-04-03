448a6c44a802dbf80da35d4d7481537296a0e3bd
connor-jason <connorejason@gmail.com>
2026-04-03 17:08:37 -0400

    HF token

--- backend_docker_deploy.sh
+++ backend_docker_deploy.sh
@@ -3,10 +3,10 @@
 # clear the known hosts cache
 ssh-keygen -f ~/.ssh/known_hosts -R "[paffenroth-23.dyn.wpi.edu]:22000"
 
-ssh -i group_key -p 22000 group09@paffenroth-23.dyn.wpi.edu << 'EOF'
+# copy HF_TOKEN file to the VM
+scp -o StrictHostKeyChecking=no -i group_key -P 22000 HF_TOKEN group09@paffenroth-23.dyn.wpi.edu:~/Case-Study-2/HF_TOKEN
 
-# stop existing systemd service if running
-systemctl stop backend 2>/dev/null || true
+ssh -o StrictHostKeyChecking=no -i group_key -p 22000 group09@paffenroth-23.dyn.wpi.edu << 'EOF'
 
 # stop and remove old container if it exists
 docker stop cs3-backend 2>/dev/null || true
@@ -21,14 +21,14 @@ else
     cd ~/Case-Study-2
 fi
 
-# remove after wemerge to main
+# remove after we merge to main
 git switch docker
 
 # build the backend image from repo root
 docker build -t cs3-backend -f backend/Dockerfile .
 
 # read HF_TOKEN
-HF_TOKEN=$(cat HF_TOKEN)
+source HF_TOKEN
 
 # run the container
 docker run -d --name cs3-backend --restart always -p 22092:22092 -e HF_TOKEN="$HF_TOKEN" -v cs3-backend-uploads:/opt/app/uploads -v cs3-backend-db:/opt/app/db cs3-backend
